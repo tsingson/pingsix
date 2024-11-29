b5824c5f9b9693184874b466f5495cd68ad6b556
qinshen <tsingson@gmail.com>
2024-11-29 15:35:12 +0800

    fix log in debug/release mode

--- src/logs/log.rs
+++ src/logs/log.rs
@@ -2,8 +2,11 @@ use std::error::Error;
 use std::path::PathBuf;
 use std::sync::Arc;
 
+#[cfg(not(debug_assertions))]
+use spdlog::prelude::*;
 use spdlog::sink::{AsyncPoolSink, RotatingFileSink, RotationPolicy};
 
+#[cfg(debug_assertions)]
 pub fn init_logger(path_buf: PathBuf) -> Result<(), Box<dyn Error>> {
     let file_sink = Arc::new(
         RotatingFileSink::builder()
@@ -20,5 +23,26 @@ pub fn init_logger(path_buf: PathBuf) -> Result<(), Box<dyn Error>> {
     })?;
 
     spdlog::set_default_logger(new_logger);
+    Ok(())
+}
+
+#[cfg(not(debug_assertions))]
+pub fn init_logger(path_buf: PathBuf) -> Result<(), Box<dyn Error>> {
+    let file_sink = Arc::new(
+        RotatingFileSink::builder()
+            .base_path(path_buf)
+            .rotation_policy(RotationPolicy::Daily { hour: 0, minute: 0 })
+            .build()?,
+    );
+    // AsyncPoolSink is a combined sink which wraps other sinks
+    let async_pool_sink = Arc::new(AsyncPoolSink::builder().sink(file_sink).build()?);
+
+    let async_logger = Arc::new(
+        Logger::builder()
+            .sink(async_pool_sink)
+            .flush_level_filter(LevelFilter::All)
+            .build()?,
+    );
+    spdlog::set_default_logger(async_logger);
     Ok(())
 }
